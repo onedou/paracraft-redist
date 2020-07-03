b4740244b2ecb830422e88e166d8ca552264216c
onedou <onedou@126.com>
2020-07-03 09:08:50 +0800

    update

--- Mod/WorldShare/service/KeepworkService/Session.lua
+++ Mod/WorldShare/service/KeepworkService/Session.lua
@@ -470,8 +470,6 @@ function KeepworkServiceSession:CheckTokenExpire(callback)
     local token = Mod.WorldShare.Store:Get('user/token')
     local info = self:LoadSigninInfo()
 
-    echo(info, true)
-
     local tokenExpire = info and info.tokenExpire or 0
 
     local function ReEntry()
@@ -525,7 +523,6 @@ function KeepworkServiceSession:CheckTokenExpire(callback)
 end
 
 function KeepworkServiceSession:RenewToken()
-    echo("from renew token!!!!!", true)
     self:CheckTokenExpire()
 
     Mod.WorldShare.Utils.SetTimeOut(function()
