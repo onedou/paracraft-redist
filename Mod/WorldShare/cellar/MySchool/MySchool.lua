bb81af0593d02600986b5a0bc24e7aa1be314664
onedou <onedou@126.com>
2020-07-03 10:11:25 +0800

    update

--- Mod/WorldShare/cellar/MySchool/MySchool.lua
+++ Mod/WorldShare/cellar/MySchool/MySchool.lua
@@ -16,36 +16,7 @@ local KeepworkService = NPL.load("(gl)Mod/WorldShare/service/KeepworkService.lua
 local MySchool = NPL.export()
 
 function MySchool:Show()
-    local function showpage()
-        local params = Mod.WorldShare.Utils.ShowWindow(870, 650, "Mod/WorldShare/cellar/MySchool/MySchool.html", "MySchool")
-
-        params._page:CallMethod("nplbrowser_instance", "SetVisible", true)
-    
-        params._page.OnClose = function()
-            Mod.WorldShare.Store:Remove('page/MySchoolPage')
-            params._page:CallMethod("nplbrowser_instance", "SetVisible", false)
-        end
-    end
-
-    if System.os.GetPlatform() ~= 'mac' then
-        local bStarted, site_url = NPLWebServer.CheckServerStarted(function(bStarted, site_url)
-            if not bStarted then
-                return false
-            end
-
-            NPL.load("(gl)script/apps/Aries/Creator/Game/NplBrowser/NplBrowserLoaderPage.lua");	
-            local NplBrowserLoaderPage = commonlib.gettable("NplBrowser.NplBrowserLoaderPage");	
-            NplBrowserLoaderPage.Check()
-            if not NplBrowserLoaderPage.IsLoaded() then
-                ParaGlobal.ShellExecute("open", MySchool.GetUrl(), "", "", 1);	
-                return
-            end
-            
-            showpage()
-        end)
-    else
-        showpage()
-    end
+    local params = Mod.WorldShare.Utils.ShowWindow(800, 400, "Mod/WorldShare/cellar/MySchool/MySchool.html", "MySchool")
 end
 
 function MySchool:SetPage()
@@ -57,15 +28,5 @@ function MySchool:Close()
 
     if MySchoolPage then
         MySchoolPage:CloseWindow()
-    end
-end
-
-function MySchool.GetUrl()
-    local token = Mod.WorldShare.Store:Get("user/token") or ''
-
-    if System.os.GetPlatform() == 'mac' or System.os.GetPlatform() == 'android' then
-        return KeepworkService:GetKeepworkUrl() .. '/p/org/home?type=protocol&port=8099&token=' .. token
-    else
-        return KeepworkService:GetKeepworkUrl() .. '/p/org/home?port=8099&token=' .. token
     end
 end
