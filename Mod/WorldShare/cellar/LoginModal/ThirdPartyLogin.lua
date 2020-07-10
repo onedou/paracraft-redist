bd98721f6acc69eafdd8948ea025d801879e4e9e
onedou <onedou@126.com>
2020-07-10 11:11:29 +0800

    update

--- Mod/WorldShare/cellar/LoginModal/ThirdPartyLogin.lua
+++ Mod/WorldShare/cellar/LoginModal/ThirdPartyLogin.lua
@@ -113,15 +113,11 @@ function ThirdPartyLogin:Init(thirdPartyType, callback)
     
             self.port = siteUrl:match("%:(%d+)") or self.port
 
-            echo(Cef3Manager.bLoaded, true)
-
             if Cef3Manager.bLoaded then
                 Handle()
             else
                 Mod.WorldShare.MsgBox:Show(L"请稍后...", 20000, nil, nil, nil, 6)
                 Cef3Manager:Connect("finishLoadCef3", nil, function()
-                    echo("on download cef3 finish!!!!", true)
-
                     Mod.WorldShare.MsgBox:Close()
                     Handle()
                 end, "UniqueConnection")
