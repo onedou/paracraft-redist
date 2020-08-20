9d5b1fbdb2ca2bca9e08c1022a416e41699e6985
onedou <onedou@126.com>
2020-08-20 16:31:35 +0800

    update

--- Mod/WorldShare/cellar/VipNotice/VipNotice.lua
+++ Mod/WorldShare/cellar/VipNotice/VipNotice.lua
@@ -12,34 +12,43 @@ local VipNotice = NPL.load("(gl)Mod/WorldShare/cellar/VipNotice/VipNotice.lua")
 
 local KeepworkService = NPL.load("(gl)Mod/WorldShare/service/KeepworkService.lua")
 local LoginModal = NPL.load("(gl)Mod/WorldShare/cellar/LoginModal/LoginModal.lua")
+local UserInfo = NPL.load("(gl)Mod/WorldShare/cellar/UserConsole/UserInfo.lua")
 
 local VipNotice = NPL.export()
 
-function VipNotice:Init(callback)
-    self.callback = callback
+VipNotice.onlyRecharge = false;
+function VipNotice:Init(bEnable, callback)
+    commonlib.echo("VipNotice:Init");
+    VipNotice.callback = callback
 
     if not KeepworkService:IsSignedIn() then
         Mod.WorldShare.Store:Set("user/loginText", L"您需要登录并成为VIP用户，才能使用此功能")
         LoginModal:Init(function(bSuccesed)
             if bSuccesed then
-                self:CheckVip()
+                self:CheckVip(bEnable)
             end
         end)
     else
-        self:CheckVip()
+        self:CheckVip(bEnable)
     end
 end
 
-function VipNotice:CheckVip()
-    if not Mod.WorldShare.Store:Get('user/isVip') then
+function VipNotice:CheckVip(bEnable)
+    commonlib.echo("VipNotice:CheckVip");
+    if (not Mod.WorldShare.Store:Get('user/isVip') or bEnable) then
+        VipNotice.onlyRecharge = bEnable;
         self:ShowPage()
     else
         if type(self.callback) == "function" then
-            self.callback()
+            VipNotice.callback()
         end
     end
 end
 
 function VipNotice:ShowPage()
-    Mod.WorldShare.Utils.ShowWindow(0, 0, "Mod/WorldShare/cellar/VipNotice/VipNotice.html", "VipNotice", 0, 0, "_fi", false, 10)
+    Mod.WorldShare.Utils.ShowWindow(0, 0, "Mod/WorldShare/cellar/VipNotice/VipNotice.html", "VipNotice", 0, 0, "_fi", false, 101)
+end
+
+function VipNotice:RefreshVipInfo()
+    UserInfo:LoginWithToken(VipNotice.callback);
 end
